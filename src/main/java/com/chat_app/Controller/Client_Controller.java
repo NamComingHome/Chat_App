84f1aac1422e49e894288cea62432720ba92e4c7
Tran Nam <nambqhg12@gmail.com>
2025-06-07 19:27:50 +0700

    Fix successful

--- src/main/java/com/chat_app/Controller/Client_Controller.java
+++ src/main/java/com/chat_app/Controller/Client_Controller.java
@@ -5,8 +5,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ListCell;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,7 +14,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.URL;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class Client_Controller implements Initializable {
@@ -38,33 +37,54 @@ public class Client_Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-//        Image img = new Image(Objects.requireNonNull(getClass().getResourceAsStream("img/send.png")));
-//        ImageView icon = new ImageView(img);
-//        icon.setFitHeight(16);
-//        icon.setFitWidth(16);
-//        btnSend.setGraphic(icon);
         try{
             Socket socket = new Socket("localhost",99);
             out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             lbStatus.setText("Connected");
             new Thread(() ->{
-               try{
-                   String msg;
-                   while ((msg = in.readLine()) != null) {
-                       String finalMsg = msg;
-                       Platform.runLater(() -> chatbox.getItems().add(finalMsg + "\n"));
-                       System.out.println(finalMsg);
-                   }
-               }catch (IOException e){
-                   e.printStackTrace();
-               }
+                try{
+                    String msg;
+                    while ((msg = in.readLine()) != null) {
+                        String finalMsg = "You:"+ msg;
+                        Platform.runLater(() -> chatbox.getItems().add(finalMsg + "\n"));
+                        System.out.println(finalMsg);
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }).start();
         }catch (Exception e) {
             e.printStackTrace();
         }
+        //custom đoạn chat
+        chatbox.setCellFactory(list -> new ListCell<>() {
+            @Override
+            protected void updateItem(String msg, boolean empty) {
+                super.updateItem(msg, empty);
+                if (empty || msg == null) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    String[] message = msg.split(":");
+                    Label label = new Label(message[1]);
+                    label.setWrapText(true);
+
+                    if (message[0].equals("You")) {
+                        label.setStyle("-fx-background-color: lightgreen; -fx-alignment: center-right;");
+                    } else {
+                        label.setStyle("-fx-background-color: lightblue;");
+                    }
+
+                    setGraphic(label);
+                }
+            }
+        });
 
     }
+
+
+
 
     @FXML
     public void actionSend(){
@@ -76,7 +96,6 @@ public class Client_Controller implements Initializable {
             chatbox.scrollTo(chatbox.getItems().size() - 1);
         }
     }
-
     @FXML
     void actionEnter(ActionEvent event) {
         String text = tfMessage.getText();
